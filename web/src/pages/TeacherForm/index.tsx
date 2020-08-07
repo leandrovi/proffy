904d5758255a099dc4682f7b781198a3bce7d95e
leandrovi <leandrofernandesvieira@gmail.com>
2020-08-07 15:27:29 -0300

    feat: teacher form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
 function TeacherForm() {
   return (
-    <h1>Teacher form</h1>
+
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </div>
   )
 }
 
