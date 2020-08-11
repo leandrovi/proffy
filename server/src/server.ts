00f70ee7bf63b615814611f4f57556a96178a478
leandrovi <leandrofernandesvieira@gmail.com>
2020-08-11 14:07:43 -0300

    feat: creating a class

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => response.json({ ok: true }))
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('\n***** 🚀 Server started on port 3333 *****\n')
